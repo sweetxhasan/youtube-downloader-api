1dc5d2e8cc7880094aef09adc12a508ae4c49c32
sweetxhasan <hasanking25809560@gmail.com>
2025-12-11 18:00:18 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
